afa5ee0dda35ad602ef71b02928c0d160fab8815
muhdarifrawi <muhdarifrawi@gmail.com>
2021-01-20 21:40:48 +0800

    added new code.

--- script.js
+++ script.js
@@ -23,5 +23,20 @@ console.log(areaOfTriangle(5,10));
 // create a program that would ask user for the cost of item
 // it would then put in the value into a function that would calculate the total cost of the item
 // inclusive of GST of 7% and a service tax of 3%
+const prompt = require("prompt-sync")();
+let itemCost = prompt("how much does your item cost? >>> $");
 
+itemCost = parseFloat(itemCost);
 
+function calculateTotal(itemCost){
+    let gst = itemCost * 0.7;
+    let svcTax = itemCost * 0.3;
+    let total = itemCost + gst + svcTax;
+    return total;
+}
+
+console.log("Total cost is: $", calculateTotal());
+
+// in programming, the numbers won't stop at a specific decinmal places unless specified.
+console.log("an example of what happens when a number is divided in a program");
+console.log("see here:", 3/0.7);
