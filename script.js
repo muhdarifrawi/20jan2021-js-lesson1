59f624433f4feebae275dd19465710193d866ac7
muhdarifrawi <muhdarifrawi@gmail.com>
2021-01-20 20:30:26 +0800

    intial commit.

---
+++ script.js
@@ -0,0 +1,27 @@
+// python function
+// def foobar():
+
+function functionName(a,b){
+    c = a + b
+    console.log(c);
+    
+}
+
+console.log("total is: ",functionName(12, 10));
+
+// create a function that calculates the area of triangle. 
+// The function should only contain the formula for area of triangle
+// show the result of the function in the terminal.
+
+function areaOfTriangle(base,height){
+    return 0.5*base*height
+}
+
+console.log(areaOfTriangle(5,10));
+
+
+// create a program that would ask user for the cost of item
+// it would then put in the value into a function that would calculate the total cost of the item
+// inclusive of GST of 7% and a service tax of 3%
+
+
